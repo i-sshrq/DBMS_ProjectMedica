70127aee53cb644df50844b8fe3eb53c0421f1c2
i-sshrq <i_ssh_rq@protonmail.com>
2020-03-31 00:22:14 +0600

    Added sql

---
+++ inititalize_entities.sql
@@ -0,0 +1,18 @@
+create table Patient
+(
+Patient_id varchar2(15)CONSTRAINT Patient_PATIENT_ID_pk PRIMARY KEY,
+Birth_reg varchar2(25) NOT NULL,
+CONSTRAINT Patient_Birth_reg UNIQUE(Birth_reg),
+patient_Name VARCHAR2(50),
+passport_number varchar2(15),
+CONSTRAINT Patient_PASSPORT_NUMBER UNIQUE(passport_number),
+blood_group varchar2(5),
+patient_DOB date,
+health_issues varchar2(200),
+other_info varchar2(200),
+address varchar2(80),
+phone_number varchar2(15),
+email varchar2(20)
+);
+
+commit;
